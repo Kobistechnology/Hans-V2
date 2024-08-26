25b1873481b235efe5c328d34962fcd9ba40c2bd
Kobistechnology <peteroghenekobiruo4@gmail.com>
2024-08-26 20:49:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,34 +14,34 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: peteroghenekobiruo" //ur yt chanel name
+global.socialm = "GitHub: kobistech ology" //ur github or insta name
+global.location = "Africa, lagos, Nigeria" //ur location
 
 //new
-global.botname = 'HANS V2' //ur bot name
-global.ownernumber = '237696900612' //ur owner number
-global.ownername = 'HANS-V2' //ur owner name
-global.websitex = "https://youtube.com/HansTech0"
-global.wagc = "https://whatsapp.com/channel/0029VaZDIdxDTkKB4JSWUk1O"
+global.botname = 'johnmark V2' //ur bot name
+global.ownernumber = '2349026678751' //ur owner number
+global.ownername = 'johnmark' //ur owner name
+global.websitex = "https://youtube.com/peteroghenekobiruo"
+global.wagc = "https://whatsapp.com/channel/0029Vabb7YwFSAt31QUidn1k"
 global.themeemoji = ''
 global.wm = "HANS_TECH."
-global.botscript = 'https://github.com/HaroldMth/Hans-V2' //script link
+global.botscript = 'https://github.com/Kobistechnology/Hans-V2' //script link
 global.packname = "Sticker By"
-global.author = "HANS_TECH"
-global.creator = "237696900612@s.whatsapp.net"
+global.author = "KOBIS_TECH"
+global.creator = "2349026678751"
 global.xprefix = '.'
-global.premium = ["237696900612"] // Premium User
+global.premium = ["2349026678751"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.autoblocknumber = '' //set autoblock country code
+global.antiforeignnumber = '' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
